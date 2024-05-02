a8d137baa942edce3772955d17aa9b87fc116a00
Alex Tetervak <tetervak@sheridancollege.ca>
2024-05-01 21:13:31 -0400

    commented out unused code

--- app/src/main/java/ca/tetervak/dicegame/ui/util/DateFormatters.kt
+++ app/src/main/java/ca/tetervak/dicegame/ui/util/DateFormatters.kt
@@ -4,25 +4,25 @@ import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.util.*
 
-private val dateFormatter =
-    DateTimeFormatter.ofPattern("EEEE, MMMM dd, yyyy")
-
-private val timeFormatter = DateTimeFormatter.ofPattern("h:mm:ss a")
+//private val dateFormatter =
+//    DateTimeFormatter.ofPattern("EEEE, MMMM dd, yyyy")
+//
+//private val timeFormatter = DateTimeFormatter.ofPattern("h:mm:ss a")
 
 private val dateAndTimeFormatter =
     DateTimeFormatter.ofPattern("EEE, MMM dd, yyyy - h:mm:ss a")
 
-fun formatDate(date: Date): String =
-    date.toInstant()
-        .atZone(ZoneId.systemDefault())
-        .toLocalDate()
-        .format(dateFormatter)
-
-fun formatTime(date: Date): String =
-    date.toInstant()
-        .atZone(ZoneId.systemDefault())
-        .toLocalTime()
-        .format(timeFormatter)
+//fun formatDate(date: Date): String =
+//    date.toInstant()
+//        .atZone(ZoneId.systemDefault())
+//        .toLocalDate()
+//        .format(dateFormatter)
+//
+//fun formatTime(date: Date): String =
+//    date.toInstant()
+//        .atZone(ZoneId.systemDefault())
+//        .toLocalTime()
+//        .format(timeFormatter)
 
 
 fun formatDateAndTime(date: Date): String =
