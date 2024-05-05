6854caf60fb01d7b367f58e5c21b20a92cb8ee95
Alex Tetervak <tetervak@sheridancollege.ca>
2024-05-05 10:04:01 -0400

    improved the unit testing

--- app/src/test/java/ca/tetervak/dicegame/domain/RollerServiceTest.kt
+++ app/src/test/java/ca/tetervak/dicegame/domain/RollerServiceTest.kt
@@ -2,10 +2,16 @@ package ca.tetervak.dicegame.domain
 
 import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThrows
 import org.junit.Before
 import org.junit.Test
+import java.lang.IllegalArgumentException
+import kotlin.random.Random
 
 class RollerServiceTest {
+
+    private val random: Random = Random(seed = 10)
+    private val rollerService: RollerService = RollerService(random)
 
     @Before
     fun setUp() {
@@ -18,25 +24,28 @@ class RollerServiceTest {
     }
 
     @Test
-    fun getTotal() {
-        println("test getTotal()")
-        val values = listOf(4,3,5)
-        println("values = $values")
-        val total = values.sum()
-        println("total = $total")
-        val rollData = RollData(values)
-        println("rollData = $rollData")
-        assertEquals(total, rollData.total)
+    fun getRollData(){
+        for(numberOfDice: Int in 1..4){
+            for(repetition: Int in 1..5){
+                println("test getNumberOfDice($numberOfDice) repetition $repetition")
+                val rollData = rollerService.getRollData(numberOfDice)
+                println("rollData = $rollData")
+                assertEquals(numberOfDice, rollData.numberOfDice)
+                val values = rollData.values
+                for(value in values){
+                    assert(value > 0)
+                    assert(value <= 6)
+                }
+            }
+        }
     }
 
     @Test
-    fun getNumberOfDice() {
-        println("test numberOfDice()")
-        val values = listOf(4,3,5,6)
-        val numberOfDice = values.size
-        println("numberOfDice = $numberOfDice")
-        val rollData = RollData(values)
-        println("rollData = $rollData")
-        assertEquals(numberOfDice, rollData.numberOfDice)
+    fun getRollData_ZeroDice(){
+        println("test throwing exception for getRollData(numberOfDice = 0)")
+        assertThrows(IllegalArgumentException::class.java) {
+            val rollData = rollerService.getRollData(numberOfDice = 0)
+            println("rollData = $rollData")
+        }
     }
 }
