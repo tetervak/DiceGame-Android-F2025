f31637c9bc2e909c82a8b7220169632728b5df0d
Alex Tetervak <tetervak@sheridancollege.ca>
2024-11-28 13:43:53 -0500

    fixed some warnings

--- app/src/main/java/ca/tetervak/dicegame/ui/theme/Theme.kt
+++ app/src/main/java/ca/tetervak/dicegame/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package ca.tetervak.dicegame.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
